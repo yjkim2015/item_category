12291492e02e5e8355a0d7d37993affd9c72fc53
김윤제 <forteas2003@hanmail.net>
2024-12-01 07:55:54 +0900

    feat: 멀티모듈 설정

--- api/src/main/java/com/musinsa/category/ApiApplication.java
+++ api/src/main/java/com/musinsa/category/ApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CategoryApplication {
+public class ApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CategoryApplication.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 
 }
--- infra/src/test/java/com/musinsa/category/InfraApplicationTests.java
+++ infra/src/test/java/com/musinsa/category/InfraApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CategoryApplicationTests {
+class InfraApplicationTests {
 
     @Test
     void contextLoads() {
